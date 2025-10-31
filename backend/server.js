c8e2e8238b02b3fe805b6cde4d74af39dd38e250
Saurav Kumar Verma <121866975+Sauravkumarv@users.noreply.github.com>
2025-10-31 16:10:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,7 @@ dbConnect();
 
 const allowedOrigins = [
   "http://localhost:3000",          
-  "https://contact-manager-app-dusky.vercel.app",
-  "https://contact-manager-ix9zicijq-sauravkumarvs-projects.vercel.app"
+   "https://contact-manager-app-git-main-sauravkumarvs-projects.vercel.app"
 ];
 
 
